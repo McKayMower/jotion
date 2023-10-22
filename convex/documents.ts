c72fd700b535249597e53d0800fe837a133cd9f9
McKay Mower <mmower777@gmail.com>
2023-10-21 18:02:46 -0600

    cover image ui and removing api

--- convex/documents.ts
+++ convex/documents.ts
@@ -269,7 +269,7 @@ export const update = mutation({
 });
 
 export const removeIcon = mutation({
-  args: {id: v.id("documents")},
+  args: { id: v.id("documents") },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) throw new Error("Unauthenticated");
@@ -282,9 +282,30 @@ export const removeIcon = mutation({
     if (existingDocument.userId !== userId) throw new Error("Unauthorized");
 
     const document = await ctx.db.patch(args.id, {
-      icon: undefined
-    })
+      icon: undefined,
+    });
 
-    return document
-  }
-})
+    return document;
+  },
+});
+
+export const removeCoverImage = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Unauthenticated");
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) throw new Error("Not found");
+
+    if (existingDocument.userId !== userId) throw new Error("Unauthorized");
+
+    const document = await ctx.db.patch(args.id, {
+      coverImage: undefined,
+    });
+
+    return document;
+  },
+});
